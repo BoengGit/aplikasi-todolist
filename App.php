220eaaadc273ea5dec7560062ea8c0db0c82d49b
BoengGit <edukasiasqor@gmail.com>
2022-08-27 17:30:36 +0700

    #clear

--- App.php
+++ App.php
@@ -7,5 +7,8 @@ require_once "BusinessLogic/AddTodoList.php";
 require_once "View/ViewAddTodoList.php";
 require_once "View/ViewRemoveTodoList.php";
 require_once "View/ViewShowTodoList.php";
+require_once "Helper/Input.php";
 
 echo "Aplikasi todolist" . PHP_EOL;
+
+viewShowTodoList();
